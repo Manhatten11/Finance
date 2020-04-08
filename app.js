ab4fdb064f9e69b8d401a00e6e0bf5de57e33faf
Manhatten11 <nyamjargal_p@yahoo.com>
2020-04-08 18:33:34 +1000

    Buh uil ajillagaag ehluuleh function-g beltgene

--- app.js
+++ app.js
@@ -1,28 +1,61 @@
 // Дэлгэцтэй ажиллах контроллер
-var uiController = (function () {})();
+var uiController = (function () {
+  var DOMstrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    addBtn: ".add__btn",
+  };
+
+  return {
+    getInput: function () {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value,
+      };
+    },
+
+    getDOMstrings: function () {
+      return DOMstrings;
+    },
+  };
+})();
 
 // Санхүүтэй ажиллах контроллер
 var financeController = (function () {})();
 
 // Програмын холбогч контроллер
 var appController = (function (uiController, financeController) {
+  var ctrlAddItem = function () {
+    // 1. Оруулах өгөгдлүүдийг дэлгэцээс олж авна.
+    console.log(uiController.getInput());
+    // 2. Олж авсан өгөгдлүүдээ санхүүгийн контроллерт дамжуулж тэнд хадгална.
+    // 3. Олж авсан өгөгдлүүдээ вэб дээр тохирох хэсэгт нь гаргана.
+    // 4. Төсвийг тооцоолно.
+    // 5. Эцсийн үлдэгдэл, тооцоог дэлгэцэнд гаргана.
+  };
 
-  var ctrlAddItem = function() {
-        // 1. Оруулах өгөгдлүүдийг дэлгэцээс олж авна.
-        console.log("Дэлгэцээс өгөгдөл авах хэсэг");
-        // 2. Олж авсан өгөгдлүүдээ санхүүгийн контроллерт дамжуулж тэнд хадгална.
-        // 3. Олж авсан өгөгдлүүдээ вэб дээр тохирох хэсэгт нь гаргана.
-        // 4. Төсвийг тооцоолно.
-        // 5. Эцсийн үлдэгдэл, тооцоог дэлгэцэнд гаргана.
-  }
+  var setupEventListener = function () {
+    var DOM = uiController.getDOMstrings();
 
-  document.querySelector(".add__btn").addEventListener("click", function () {
-    ctrlAddItem();
-  });
-
-  document.addEventListener("keypress", function (event) {
-    if (event.keyCode === 13 || event.which === 13) {
+    document.querySelector(DOM.addBtn).addEventListener("click", function () {
       ctrlAddItem();
-    }
-  });
+    });
+
+    document.addEventListener("keypress", function (event) {
+      if (event.keyCode === 13 || event.which === 13) {
+        ctrlAddItem();
+      }
+    });
+  };
+
+  return {
+    init: function () {
+      console.log("Application started....");
+      setupEventListener();
+    },
+  };
 })(uiController, financeController);
+
+appController.init();
